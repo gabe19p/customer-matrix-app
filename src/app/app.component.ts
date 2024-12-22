d61af55d44a218753da293fe82a1b26ea96c62b3
gabe19p <g19purselley@gmail.com>
2024-12-22 16:36:05 +0900

    main-layout w/ expandable nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatCardModule,
+    CommonModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'customer-matrix-app';
+  isExpanded = true; // boolean for the left nav
 }

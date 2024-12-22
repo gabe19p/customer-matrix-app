106890db4cc9f7e97a6eb66f4a7b4b12ecc34004
gabe19p <g19purselley@gmail.com>
2024-12-22 17:03:14 +0900

    deployment attempt

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,9 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
+
+// npm run build -- --output-path=dist/customer-matrix-app --base-href /customer-matrix-app/
+// ng deploy --base-href=https://gabe19p.github.io/customer-matrix-app/
